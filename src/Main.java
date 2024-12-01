1963e7695f98ee4fb8ef39f58e0bd42478f612db
deadsnxw <maks.roskoshenko@gmail.com>
2024-12-01 13:28:18 +0200

    usage of music album

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.List;
+
+/**
+ * Main class for testing the Album and Music Composition hierarchy.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            Album album = new Album();
+
+            album.addComposition(new RockComposition("Bohemian Rhapsody", "Queen", 5.55));
+            album.addComposition(new JazzComposition("Take Five", "Dave Brubeck", 5.24));
+            album.addComposition(new PopComposition("Thriller", "Michael Jackson", 5.57));
+            album.addComposition(new RockComposition("Stairway to Heaven", "Led Zeppelin", 8.02));
+            album.addComposition(new JazzComposition("So What", "Miles Davis", 9.22));
+
+            System.out.println("Album before sorting:");
+            album.printAlbum();
+
+            System.out.println("\nTotal duration of the album: " + album.calculateTotalDuration() + " minutes");
+
+            System.out.println("\nSorting compositions by genre...");
+            album.sortCompositionsByGenre();
+            album.printAlbum();
+
+            System.out.println("\nFinding compositions with duration between 5 and 6 minutes:");
+            List<MusicComposition> filteredCompositions = album.findCompositionsByDurationRange(5, 6);
+            filteredCompositions.forEach(System.out::println);
+
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
